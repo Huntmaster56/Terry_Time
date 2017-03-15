9b6771f742da5d3846c596ec5fea1e331a25b4fa
joshua.livingst <joshua.livingst@SEA-8CC916>
2017-03-15 10:51:46 -0700

    terry did stuff

---
+++ Assets/Scripts/LoadOnTriger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadOnTriger : MonoBehaviour
+{
+    public string levelName;
+
+    void OnTriggerEnter(Collider other)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(levelName);
+    }
+}
